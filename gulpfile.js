020ed6f65f3297b9e2073bc7b6d177a86ddf69ca
Zzbaivong <Zzbaivong@gmail.com>
2016-10-17 15:40:15 +0700

    Split scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 /* eslint-env node */
 
 // Load Gulp
-var gulp = require('gulp'),
+var fs = require('fs'),
+    path = require('path'),
+    gulp = require('gulp'),
     changed = require('gulp-changed'),
     browserSync = require('browser-sync')
     .create(),
@@ -31,13 +33,9 @@ var gulp = require('gulp'),
         ''
     ].join('\n'),
 
-    path = {
+    pjPath = {
         public: 'public/' + pkg.version,
-        js: {
-            all: 'src/assets/**/*.js',
-            top: 'src/assets/js/top/*.js',
-            bottom: 'src/assets/js/bottom/*.js'
-        },
+        js: 'src/assets/scripts/',
         less: 'src/assets/less/**/*.less',
         tpl: 'src/templates/**/*.tpl'
     };
@@ -45,7 +43,7 @@ var gulp = require('gulp'),
 
 // npm run gulp stylelint
 gulp.task('stylelint', function() {
-    return gulp.src(path.less)
+    return gulp.src(pjPath.less)
         .pipe(stylelint({
             failAfterError: false,
             reporters: [{
@@ -73,7 +71,7 @@ gulp.task('less', function() {
             .pipe(header(banner, {
                 pkg: pkg
             }))
-            .pipe(gulp.dest(path.public))
+            .pipe(gulp.dest(pjPath.public))
             .pipe(gulp.dest('dist'))
             .pipe(browserSync.stream());
     }
@@ -84,78 +82,103 @@ gulp.task('less', function() {
     return merge(forum, chatbox);
 });
 
+// npm run gulp css
+gulp.task('css', function() {
+    return gulp.src('src/assets/css/mobile.css')
+        .pipe(changed('css'))
+        .pipe(gulp.dest(pjPath.public))
+        .pipe(gulp.dest('dist'));
+});
+
 
 // npm run gulp eslint
 gulp.task('eslint', function() {
-    return gulp.src([path.js.top, path.js.bottom])
+    return gulp.src(pjPath.js + '**/*.js')
         .pipe(changed('dist'))
         .pipe(eslint())
         .pipe(eslint.format());
 });
 
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function(file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+
 // npm run gulp js
 gulp.task('js', function() {
-    function buildJs(input, output) {
-        return gulp.src(input)
-            .pipe(uglify({
-                output: {
-                    'ascii_only': true
-                }
-            }))
-            .pipe(concat(pkg.name + '.' + output + '.js'))
-            .pipe(header(banner, {
-                pkg: pkg
-            }))
-            .pipe(gulp.dest(path.public))
-            .pipe(gulp.dest('dist'));
-    }
+    var folders = getFolders(pjPath.js),
 
-    var jsTop = buildJs(path.js.top, 'top'),
-        jsBottom = buildJs(path.js.bottom, 'bottom');
+        tasks = folders.map(function(folder) {
+            return gulp.src(pjPath.js + folder + '/*.js')
+                .pipe(uglify({
+                    output: {
+                        'ascii_only': true
+                    }
+                }))
+                .pipe(concat(pkg.name + '.' + folder + '.js'))
+                .pipe(header(banner, {
+                    pkg: pkg
+                }))
+                .pipe(gulp.dest(pjPath.public))
+                .pipe(gulp.dest('dist'));
+        });
 
-    return merge(jsTop, jsBottom);
+    return merge(tasks);
 });
 
 
 // npm run gulp prezip
 gulp.task('prezip', function() {
-    var rawgitPath = config.cdn + '/baivong/bubcloud/master/' + path.public,
+    function changeJsPath(tplName, fileName) {
+        return gulp.src('src/templates/General/' + tplName + '.tpl')
+            .pipe(changed('dist'))
+            .pipe(replace('/bubcloud.' + fileName + '.js', rawgitPath + '/bubcloud.' + fileName + '.js'))
+            .pipe(gulp.dest('dist/templates/General/'));
+    }
+
+    var rawgitPath = config.cdn + '/baivong/bubcloud/master/' + pjPath.public,
 
         headerTpl = gulp.src('src/templates/General/overall_header.tpl')
         .pipe(changed('dist'))
         .pipe(replace('/style.css', rawgitPath + '/style.css'))
-        .pipe(replace('/bubcloud.top.js', rawgitPath + '/bubcloud.top.js'))
+        .pipe(replace('/bubcloud.header.js', rawgitPath + '/bubcloud.header.js'))
         .pipe(gulp.dest('dist/templates/General/')),
 
-        footerTpl = gulp.src('src/templates/General/overall_footer_end.tpl')
-        .pipe(changed('dist'))
-        .pipe(replace('/bubcloud.bottom.js', rawgitPath + '/bubcloud.bottom.js'))
-        .pipe(gulp.dest('dist/templates/General/')),
+        footerTpl = changeJsPath('overall_footer_end', 'footer'),
+        indexTpl = changeJsPath('index_body', 'index'),
+        topicTpl = changeJsPath('viewtopic_body', 'topic'),
 
-        otherTpl = gulp.src([path.tpl, '!src/templates/General/overall_header.tpl', '!src/templates/General/overall_footer_end.tpl'], {
+        otherTpl = gulp.src([pjPath.tpl, '!src/templates/General/overall_header.tpl', '!src/templates/General/overall_footer_end.tpl'], {
             base: './src/'
         })
         .pipe(changed('dist'))
+        .pipe(gulp.dest('dist')),
+
+        mobileTpl = gulp.src('src/templates/Mobile version/overall_header.tpl')
+        .pipe(changed('dist'))
+        .pipe(replace('/mobile.css', rawgitPath + '/mobile.css'))
         .pipe(gulp.dest('dist'));
 
-    return merge(headerTpl, footerTpl, otherTpl);
+    return merge(headerTpl, footerTpl, indexTpl, topicTpl, otherTpl, mobileTpl);
 });
 
 // npm run zip
 // npm run gulp zip
 gulp.task('zip', ['prezip'], function() {
     return gulp.src('dist/templates/**/*.tpl')
-        .pipe(changed(path.public))
+        .pipe(changed(pjPath.public))
         .pipe(zip('forumotion.' + pkg.name + '.zip'))
-        .pipe(gulp.dest(path.public));
+        .pipe(gulp.dest(pjPath.public));
 });
 
 
 // npm run gulp watch
 gulp.task('watch', function() {
-    gulp.watch(path.js.all, ['js']).on('change', browserSync.reload);
-    gulp.watch(path.less, ['less']);
-    gulp.watch(path.tpl, ['zip']);
+    gulp.watch(pjPath.js + '**/*.js', ['js']).on('change', browserSync.reload);
+    gulp.watch(pjPath.less, ['less']);
+    gulp.watch(pjPath.tpl, ['zip']);
 });
 
 // npm run gulp
@@ -167,10 +190,10 @@ gulp.task('lint', ['eslint', 'stylelint']);
 
 // npm run build
 // npm run gulp build
-gulp.task('build', ['lint', 'less', 'js', 'zip'], function() {
+gulp.task('build', ['lint', 'less', 'js', 'zip', 'css'], function() {
     return gulp.src('bin/*.bbtheme')
-        .pipe(changed(path.public))
-        .pipe(gulp.dest(path.public));
+        .pipe(changed(pjPath.public))
+        .pipe(gulp.dest(pjPath.public));
 });
 
 
