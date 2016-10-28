3c62dff587e3abbfcc1758c9452f6fb9c011556b
Zzbaivong <Zzbaivong@gmail.com>
2016-10-28 20:05:25 +0700

    Fix Id duplicated

--- src/assets/scripts/topic/toc.js
+++ src/assets/scripts/topic/toc.js
@@ -8,6 +8,13 @@
         });
     }
 
+    function updateId(id) {
+        if (!$(id).length) return id;
+
+        id += '-i';
+        return updateId(id);
+    }
+
 
     $('.post').each(function() {
         var $post = $(this),
@@ -18,8 +25,10 @@
         var $fly = createDiv('toc-fly'),
             $control = createDiv('toc-control').html('<i class="fa fa-bars" aria-hidden="true"></i>'),
             $list = createDiv('toc-list'),
+            $totop = createDiv('toc-totop'),
 
             $win = $(window),
+            winHeight = 0,
             postHeadHeight = 131,
             postTop = 0,
             postFootTop = 0,
@@ -40,16 +49,18 @@
             }, 250),
 
             checkSize = debounce(function() {
+                winHeight = $win.height() - 30;
                 postTop = $post.offset().top;
                 postFootTop = $post.find('.post-footer').offset().top - 2;
                 tocHeight = $fly.height();
                 tocTop = postTop + postHeadHeight;
 
+                $list.css('max-height', winHeight);
                 tocPosition();
             }, 250);
 
 
-        $list.append($('<a>', {
+        $totop.append($('<a>', {
             class: 'toc-link toc-top',
             href: '#' + $post.attr('id'),
             html: '<i class="fa fa-arrow-up" aria-hidden="true"></i> Lên đầu bài viết '
@@ -59,18 +70,20 @@
             var _this = this,
                 $h = $(_this),
                 str = _this.textContent,
-                id = bodau(str);
+                hash = '#' + bodau(str);
+
+            hash = updateId(hash);
 
             $h.append($('<a>', {
                 class: 'toc-link toc-anchor',
-                href: '#' + id,
-                id: id,
+                href: hash,
+                id: hash.slice(1),
                 html: '<i class="fa fa-anchor" aria-hidden="true"></i>'
             }));
 
             $list.append($('<a>', {
                 class: 'toc-link toc-' + _this.tagName.toLowerCase(),
-                href: '#' + id,
+                href: hash,
                 text: str
             }));
         });
@@ -81,9 +94,7 @@
             checkSize();
         });
 
-        $post.append($fly.append($list).append($control));
-        tocHeight = $fly.height();
-        tocTop = $fly.offset().top;
+        $post.append($fly.append($totop.append($control)).append($list));
         checkSize();
 
         $win.on('load resize', checkSize).on('scroll', tocPosition);
